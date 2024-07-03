9d31592ecc09814023ca7b1f5affd5ad8319d296
zyg <1519881356@qq.com>
2024-07-03 15:12:46 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <TheWelcome />
+    <van-button type="primary">主要按钮</van-button>
+  </main>
+</template>
+
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
